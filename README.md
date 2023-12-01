cddce6d6a20eae45921f21c7ecfc42e2fb37f27e
Rylandfree <rylandfreeman@aeonai.com>
2023-12-01 15:57:32 -0700

    fixed import error, added route for getting all questions, added to readme, put database initialization file

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # UVU_cognitive_assessment
+
+To start up, go to the docker-compose.yml file and input the username, password, and port you want\
+to use for the database. If you have no other local postgres instances on port 5432, you can delete\ 
+the ```command: -p 5434``` section and keep
+```
+ports:
+  - "5432:5432"
+```
+Otherwise, set the port numbers to what you want it to redirect to.\
+Then, in a terminal, run\
+```docker compose up flask_db```\
+Check that it is running fine, it should say it is ready to accept connections at the bottom.\
+Then, in a new terminal, run\
+```docker compose build```\
+You should now be able to see the container with\
+```docker images```\
+Final, run\
+```docker compose up flask_app```\
+And everything should be up and running.\
+When building subsequent times, you only need\
+```docker compose up flask_db``` and ```docker compose up flask_app```
+
+If it is your first time running the database, paste the sql commands in the database_init.sql
+file into postgres console.
--- app.py
+++ app.py
@@ -1,21 +1,24 @@
-from flask import Flask, request, jsonify, make_response
+from dataclasses import dataclass
+
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from os import environ
-from sqlalchemy.dialects.postgresql import JSON, JSONB, insert 
+
+from sqlalchemy import Integer, String
+from sqlalchemy.orm import Mapped, mapped_column
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = environ.get('DB_URL')
 db = SQLAlchemy(app)
 
-class Test(db.Model):
-    __tablename__ = 'Test'
-    id = db.Column(db.Integer, primary_key=True)
-    questions = db.Column(JSONB, unique=False, nullable=False)
-    
-    #def json(self):
-    #    return {'id': self.id,'username': self.username, 'email': self.email}
-    
-db.create_all()
+
+@dataclass
+class Questions(db.Model):
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    group_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    title: Mapped[str] = mapped_column(String)
+    content: Mapped[str] = mapped_column(String)
+
 
 # Check if app is running
 @app.route('/')
@@ -23,67 +26,12 @@ def hello_world():
     return 'Hello World!'
 
 
-# create a test
-@app.route('/tests', methods=['POST'])
-def create_test():
-  try:
-    data = request.get_json()
-    new_test = Test(questions=data)
-    db.session.add(new_test)
-    db.session.commit()
-    return make_response(jsonify({'message': 'test created'}), 201)
-  except Exception as e:
-    print(e)
-    return make_response(print(e), 500)
-  
-# get all tests
-@app.route('/tests', methods=['GET'])
-def get_test():
-  try:
-    tests = Test.query.all()
-    return make_response(jsonify([test.questions for test in tests]), 200)
-  except Exception as e:
-    return make_response(jsonify({'message': str(e)}), 500)
-  
-''' Can be implemented later if needed
-# get a user by id
-@app.route('/users/<int:id>', methods=['GET'])
-def get_user(id):
-  try:
-    user = User.query.filter_by(id=id).first()
-    if user:
-      return make_response(jsonify({'user': user.json()}), 200)
-    return make_response(jsonify({'message': 'user not found'}), 404)
-  except Exception as e:
-    print(e)
-    return make_response(jsonify({'message': 'error getting user'}), 500)
+@app.route("/questions", methods=["GET"])
+def question_list():
+    questions = Questions.query.all()
+    return jsonify(questions)
 
-# update a user
-@app.route('/users/<int:id>', methods=['PUT'])
-def update_user(id):
-  try:
-    user = User.query.filter_by(id=id).first()
-    if user:
-      data = request.get_json()
-      user.username = data['username']
-      user.email = data['email']
-      db.session.commit()
-      return make_response(jsonify({'message': 'user updated'}), 200)
-    return make_response(jsonify({'message': 'user not found'}), 404)
-  except Exception as e:
-    print(e)
-    return make_response(jsonify({'message': 'error updating user'}), 500)
-  
-# delete a user
-@app.route('/users/<int:id>', methods=['DELETE'])
-def delete_user(id):
-  try:
-    user = User.query.filter_by(id=id).first()
-    if user:
-      db.session.delete(user)
-      db.session.commit()
-      return make_response(jsonify({'message': 'user deleted'}), 200)
-    return make_response(jsonify({'message': 'user not found'}), 404)
-  except Exception as e:
-    print(e)
-    return make_response(jsonify({'message': 'error deleting user'}), 500)'''
+
+if __name__ == '__main__':
+    db.session.commit()
+    app.run()
