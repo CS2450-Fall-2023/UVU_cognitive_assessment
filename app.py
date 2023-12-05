bfd3e774079b7158ff616ff9b03edd505377cfc5
Karenlima7 <10800221@uvu.edu>
2023-12-05 01:17:09 -0700

    endpoint (get questions by group_id) created, next_group_id field added to table file

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from dataclasses import dataclass
 
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from flask_sqlalchemy import SQLAlchemy
 from os import environ
 
@@ -16,6 +16,7 @@ db = SQLAlchemy(app)
 class Questions(db.Model):
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     group_id: Mapped[int] = mapped_column(Integer, nullable=False)
+    next_group_id: Mapped[int] = mapped_column(Integer, nullable=False)
     title: Mapped[str] = mapped_column(String)
     content: Mapped[str] = mapped_column(String)
 
@@ -30,6 +31,18 @@ def hello_world():
 def question_list():
     questions = Questions.query.all()
     return jsonify(questions)
+
+
+# get a question by group_id
+@app.route('/questions/<int:id>', methods=['GET'])
+def question_id(id):
+  try:
+    questions = Questions.query.filter_by(group_id=id).all()
+    if questions:
+      return make_response(jsonify(questions), 200)
+    return make_response(jsonify({'message': 'group id not found'}), 404)
+  except Exception as e:
+    return make_response(print(e), 500)
 
 
 if __name__ == '__main__':
